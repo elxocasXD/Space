b9482cb36251d84dac1c538a93da0d80b0ca5699
elxocasXD <115422799+elxocasXD@users.noreply.github.com>
2023-03-01 17:17:02 -0500

    Create Trip Hub Free.lua

---
+++ Trip Hub Free.lua
@@ -0,0 +1,7 @@
+--[[ 
+𝐁𝐲: Space#0234
+𝐒𝐞𝐫𝐯𝐞𝐫 𝐃𝐢𝐬𝐜𝐨𝐫𝐝: https://discord.gg/79fKXVuJfr
+]]--
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GamerEmilianoFF/encrypted/main/kavo%20free",true))()
